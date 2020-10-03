6bbefd80cf4cecdf211efc8f1ee3c5b7e2aedddb
dhruvsalunkhe <tanaya.salunkhe@gmail.com>
2020-10-03 10:57:09 +0530

    api

--- sketch.js
+++ sketch.js
@@ -7,11 +7,11 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var bg = "sprites/bg.png";
 var gameState = "onSling";
-
+var score = 0;
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImage();
 }
 
 function setup(){
@@ -45,18 +45,27 @@ function setup(){
 }
 
 function draw(){
+
+    if (backgroundImg){
     background(backgroundImg);
+    }
+    noStroke();
+    textSize(35);
+    fill("white");
+    text("score"+ score , width - 300, 50);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +94,19 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBackgroundImage(){
+var response = await fetch("http://worldtimeapi.org/api/timezone/Europe/London");
+var responseJSON = await response.json();
+var datetime = responseJSON.datetime;
+var hour = datetime.slice(11,13);
+if (hour >= 07 && hour<= 19){
+    bg = "sprites/bg.png";
+}
+else {
+
+    bg = "sprites/bg2.jpg";
+}
+backgroundImg = loadImage(bg);
 }
